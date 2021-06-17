8b8a93b0ec82cd97490d5db4a4c0b12d8a5bf3f0
theFool32 <saber.rl32@gmail.com>
2021-06-17 10:55:32 +0800

    :recycle: Use `awesome-pair` only.

--- elisp/init-parens.el
+++ elisp/init-parens.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 10:17:13 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Fri Jun  4 15:08:22 2021 (+0800)
+;; Last-Updated: Thu Jun 17 10:55:19 2021 (+0800)
 ;;           By: theFool32
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d parenthesis smartparens delete-block
@@ -40,58 +40,10 @@
 (eval-when-compile
   (require 'init-global-config))
 
-;; Automatic parenthesis pairing
-(use-package elec-pair
-  :ensure nil
-  :hook (after-init . electric-pair-mode)
-  :init (setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit))
-
-;; MatchParens
-;; Show matching parenthesis
-(show-paren-mode 1)
-;; we will call `blink-matching-open` ourselves...
-(remove-hook 'post-self-insert-hook
-             #'blink-paren-post-self-insert-function)
-
-;; this still needs to be set for `blink-matching-open` to work
-(setq blink-matching-paren 'show)
-(let ((ov nil)) ; keep track of the overlay
-  (advice-add
-   #'show-paren-function
-   :after
-   (defun show-paren--off-screen+ (&rest _args)
-     "Display matching line for off-screen paren."
-     (when (overlayp ov)
-       (delete-overlay ov))
-     ;; check if it's appropriate to show match info,
-     ;; see `blink-paren-post-self-insert-function'
-     (when (and (overlay-buffer show-paren--overlay)
-                (not (or cursor-in-echo-area
-                         executing-kbd-macro
-                         noninteractive
-                         (minibufferp)
-                         this-command))
-                (and (not (bobp))
-                     (memq (char-syntax (char-before)) '(?\) ?\$)))
-                (= 1 (logand 1 (- (point)
-                                  (save-excursion
-                                    (forward-char -1)
-                                    (skip-syntax-backward "/\\")
-                                    (point))))))
-       ;; rebind `minibuffer-message' called by
-       ;; `blink-matching-open' to handle the overlay display
-       (cl-letf (((symbol-function #'minibuffer-message)
-                  (lambda (msg &rest args)
-                    (let ((msg (apply #'format-message msg args)))
-                      (setq ov (display-line-overlay+
-                                (window-start) msg))))))
-         (blink-matching-open))))))
-;; -MatchParens
-
-
 (use-package awesome-pair
   :straight (:host github :repo "manateelazycat/awesome-pair")
-  :hook (prog-mode . awesome-pair-mode)
+  :hook ((prog-mode . awesome-pair-mode)
+         (text-mode . awesome-pair-mode))
   :config
   (define-key awesome-pair-mode-map (kbd "(") 'awesome-pair-open-round)
   (define-key awesome-pair-mode-map (kbd "[") 'awesome-pair-open-bracket)
@@ -99,26 +51,15 @@
   (define-key awesome-pair-mode-map (kbd ")") 'awesome-pair-close-round)
   (define-key awesome-pair-mode-map (kbd "]") 'awesome-pair-close-bracket)
   (define-key awesome-pair-mode-map (kbd "}") 'awesome-pair-close-curly)
-  (define-key awesome-pair-mode-map (kbd "=") 'awesome-pair-equal)
 
   (define-key awesome-pair-mode-map (kbd "%") 'awesome-pair-match-paren)
   (define-key awesome-pair-mode-map (kbd "\"") 'awesome-pair-double-quote)
 
   (define-key awesome-pair-mode-map (kbd "SPC") 'awesome-pair-space)
 
-  (define-key awesome-pair-mode-map (kbd "M-o") 'awesome-pair-backward-delete)
-  (define-key awesome-pair-mode-map (kbd "C-d") 'awesome-pair-forward-delete)
   (define-key awesome-pair-mode-map (kbd "C-k") 'awesome-pair-kill)
+  (define-key awesome-pair-mode-map (kbd "<backspace>") 'awesome-pair-backward-delete)
 
-  (define-key awesome-pair-mode-map (kbd "M-\"") 'awesome-pair-wrap-double-quote)
-  (define-key awesome-pair-mode-map (kbd "M-[") 'awesome-pair-wrap-bracket)
-  (define-key awesome-pair-mode-map (kbd "M-{") 'awesome-pair-wrap-curly)
-  (define-key awesome-pair-mode-map (kbd "M-(") 'awesome-pair-wrap-round)
-  (define-key awesome-pair-mode-map (kbd "M-)") 'awesome-pair-unwrap)
-
-  (define-key awesome-pair-mode-map (kbd "M-p") 'awesome-pair-jump-right)
-  (define-key awesome-pair-mode-map (kbd "M-n") 'awesome-pair-jump-left)
-  (define-key awesome-pair-mode-map (kbd "M-:") 'awesome-pair-jump-out-pair-and-newline)
   )
 
 (provide 'init-parens)
