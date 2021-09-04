d0b2610068d0b68142b60e9053a406cdea05ea1c
theFool32 <saber.rl32@gmail.com>
2021-09-04 20:00:22 +0800

    :children_crossing: [latex] code clean & local leader binding

--- elisp/init-latex.el
+++ elisp/init-latex.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Wed Sep  4 16:35:00 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Mon Aug 23 01:10:16 2021 (+0800)
+;; Last-Updated: Sat Sep  4 19:59:57 2021 (+0800)
 ;;           By: theFool32
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d auctex
@@ -130,42 +130,6 @@
         ("restartlist" "{")
         ("crefname" "{")))
 
-;;;###autoload
-(defun +latex/LaTeX-indent-item ()
-  "Provide proper indentation for LaTeX \"itemize\",\"enumerate\", and
-\"description\" environments.
-\"\\item\" is indented `LaTeX-indent-level' spaces relative to the the beginning
-of the environment.
-Continuation lines are indented either twice `LaTeX-indent-level', or
-`LaTeX-indent-level-item-continuation' if the latter is bound."
-  (save-match-data
-    (let* ((offset LaTeX-indent-level)
-           (contin (or (and (boundp '+latex-indent-level-item-continuation)
-                            +latex-indent-level-item-continuation)
-                       (* 4 offset)))
-           (re-beg "\\\\begin{")
-           (re-end "\\\\end{")
-           (re-env "\\(itemize\\|\\enumerate\\|description\\)")
-           (indent (save-excursion
-                     (when (looking-at (concat re-beg re-env "}"))
-                       (end-of-line))
-                     (LaTeX-find-matching-begin)
-                     (current-column))))
-      (cond ((looking-at (concat re-beg re-env "}"))
-             (or (save-excursion
-                   (beginning-of-line)
-                   (ignore-errors
-                     (LaTeX-find-matching-begin)
-                     (+ (current-column)
-                        (if (looking-at (concat re-beg re-env "}"))
-                            contin
-                          offset))))
-                 indent))
-            ((looking-at (concat re-end re-env "}"))
-             indent)
-            ((looking-at "\\\\item")
-             (+ offset indent))
-            ((+ contin indent))))))
 
 ;;;###autoload
 (defun +latex-symbols-company-backend (command &optional arg &rest _ignored)
@@ -176,50 +140,19 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
       (company-math-symbols-unicode command arg)
     (company-math-symbols-latex command arg)))
 
-
-(defvar +latex-indent-level-item-continuation 4
-  "Custom indentation level for items in enumeration-type environments")
-
-(defvar +latex-bibtex-file nil
-  "File AUCTeX (specifically RefTeX) uses to search for citations.")
-
 (defvar +latex-enable-unicode-math nil
   "If non-nil, use `company-math-symbols-unicode' backend in LaTeX-mode,
   enabling unicode symbols in math regions. This requires the unicode-math latex
   package to be installed.")
 
-(defvar +latex-viewers '(skim evince sumatrapdf zathura okular pdf-tools)
-  "A list of enabled latex viewers to use, in this order. If they don't exist,
-  they will be ignored. Recognized viewers are skim, evince, sumatrapdf, zathura,
-  okular and pdf-tools.
-  If no viewers are found, `latex-preview-pane' is used.")
-
 ;;
 (defvar +latex--company-backends nil)
-
-
-;;
-;; Packages
-
-;; (when (stringp +latex-bibtex-file)
-;;   (setq bibtex-completion-bibliography (list (expand-file-name +latex-bibtex-file))
-;;         reftex-default-bibliography bibtex-completion-bibliography))
-
 
 
 (use-package reftex
   :after tex
   :hook (LaTeX-mode . reftex-mode)
   :custom
-  (reftex-cite-format
-   '((?a . "\\autocite[]{%l}")
-     (?b . "\\blockcquote[]{%l}{}")
-     (?c . "\\cite[]{%l}")
-     (?f . "\\footcite[]{%l}")
-     (?n . "\\nocite{%l}")
-     (?p . "\\parencite[]{%l}")
-     (?s . "\\smartcite[]{%l}")
-     (?t . "\\textcite[]{%l}")))
   (reftex-plug-into-AUCTeX t)
   (reftex-toc-split-windows-fraction 0.3)
   :config
@@ -241,6 +174,7 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
   :straight auctex
   :mode ("\\.tex\\'" . LaTeX-mode)
   :custom
+  (TeX-insert-braces nil)
   (TeX-parse-self t) ; parse on load
   (TeX-auto-save t)  ; parse on save
   ;; use hidden dirs for auctex files
@@ -252,7 +186,7 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
   (TeX-source-correlate-start-server nil)
   ;; automatically insert braces after sub/superscript in math mode
   (TeX-electric-sub-and-superscript t)
-  (TeX-master t)
+  (TeX-master nil)
 
   (LaTeX-section-hook ; Add the toc entry to the sectioning hooks.
    '(LaTeX-section-heading
@@ -261,7 +195,6 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
      LaTeX-section-section
      LaTeX-section-label))
   (LaTeX-fill-break-at-separators nil)
-  (LaTeX-item-indent 0)
 
   :config
   (general-define-key :states 'normal :keymaps 'LaTeX-mode-map (kbd "zo") #'TeX-fold-clearout-item)
@@ -273,7 +206,6 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
                                 TeX-command-list)
                                (setq TeX-command-list (delete-dups TeX-command-list))
                                ))
-  ;; (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
   ;; use Skim as default pdf viewer
   ;; Skim's displayline is used for forward search (from .tex to .pdf)
   ;; option -b highlights the current line; option -g opens Skim in the background
@@ -284,8 +216,6 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
     (assq-delete-all 'output-pdf TeX-view-program-selection)
     (add-to-list 'TeX-view-program-selection '(output-pdf "PDF Viewer")))
   (setcar (cdr (assoc "Check" TeX-command-list)) "chktex -v6 -H %s")
-  (add-hook 'TeX-mode-hook (lambda() (setq ispell-parser 'tex
-                                      fill-nobreak-predicate (cons #'texmathp fill-nobreak-predicate))))
   ;; Enable word wrapping
   (add-hook 'TeX-mode-hook #'visual-line-mode)
   ;; Fold TeX macros
@@ -296,11 +226,15 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
   (when +latex--company-backends
     (set-company-backend! 'latex-mode +latex--company-backends))
 
-  ;; Provide proper indentation for LaTeX "itemize","enumerate", and
-  ;; "description" environments. See
-  ;; http://emacs.stackexchange.com/questions/3083/how-to-indent-items-in-latex-auctex-itemize-environments
-  (dolist (env '("itemize" "enumerate" "description"))
-    (add-to-list 'LaTeX-indent-environment-list `(,env +latex/LaTeX-indent-item)))
+  (defun my-TeX-compile ()
+    (interactive)
+    (TeX-master-file nil nil t)
+    (TeX-command-sequence '("BibTeX" "LaTeX" "LaTeX") t))
+  (local-leader-def
+    :keymaps 'LaTeX-mode-map
+    "m" '(TeX-master-file-ask :wk "Master file")
+    "c" '(my-TeX-compile :wk "Compile")
+    "v" '(TeX-view :wk "View"))
   )
 
 (use-package cdlatex
@@ -324,10 +258,7 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
             (lambda () (interactive)
               (define-key org-cdlatex-mode-map "`" 'asymbol-insert-text-or-symbol)))
   )
-(use-package adaptive-wrap
-  :after tex
-  :hook (LaTeX-mode . adaptive-wrap-prefix-mode)
-  :init (setq-default adaptive-wrap-extra-indent 0))
+
 (use-package company-auctex
   :after (company tex)
   :init
@@ -342,9 +273,6 @@ Continuation lines are indented either twice `LaTeX-indent-level', or
   :init
   (add-to-list '+latex--company-backends #'+latex-symbols-company-backend nil #'eq))
 ;; -AUCTeXPac
-
-(use-package magic-latex-buffer
-  :after (tex))
 
 (provide 'init-latex)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
