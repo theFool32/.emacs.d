ab6207a5cd61da9dd945dfd487de3e05b20c4f73
theFool32 <saber.rl32@gmail.com>
2021-09-19 11:40:53 +0800

    :ambulance: [smerge] bindings only for smerge-mode

--- elisp/init-magit.el
+++ elisp/init-magit.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 08:40:27 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Thu Sep  9 23:43:00 2021 (+0800)
+;; Last-Updated: Sun Sep 19 11:40:16 2021 (+0800)
 ;;           By: theFool32
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d magit
@@ -263,15 +263,16 @@ kill all magit buffers for this repo."
                           (when (re-search-forward "^<<<<<<< " nil t)
                             (smerge-mode 1))))))
   :config
-  (general-def 'normal
-    :prefix ","
-    "n" '(smerge-next :wk "Next conflict")
-    "p" '(smerge-prev :wk "Previous conflict")
-    "RET" '(smerge-keep-current :wk "Accept current")
-    "l" '(smerge-keep-lower :wk "Keep lower")
-    "u" '(smerge-keep-upper :wk "Keep upper")
-    "m" '(smerge-keep-mine :wk "Keep mine")
-    "A" '(smerge-keep-all :wk "Keep all"))
+  (general-define-key :states 'normal
+                      :keymaps 'smerge-mode-map
+                      :prefix ","
+                      "n" '(smerge-next :wk "Next conflict")
+                      "p" '(smerge-prev :wk "Previous conflict")
+                      "RET" '(smerge-keep-current :wk "Accept current")
+                      "l" '(smerge-keep-lower :wk "Keep lower")
+                      "u" '(smerge-keep-upper :wk "Keep upper")
+                      "m" '(smerge-keep-mine :wk "Keep mine")
+                      "A" '(smerge-keep-all :wk "Keep all"))
   )
 
 
