625c8da66045d5b933d3d78285a243690c9cb1e4
theFool32 <saber.rl32@gmail.com>
2022-04-01 13:08:46 +0800

    [dired] open in external app with `c-ret`

--- elisp/init-dired.el
+++ elisp/init-dired.el
@@ -63,11 +63,43 @@
   ;; Reuse same dired buffer, to prevent numerous buffers while navigating in dired
   (put 'dired-find-alternate-file 'disabled nil)
 
+  (defun xah-open-in-external-app (&optional @fname)
+    "Open the current file or dired marked files in external app.
+The app is chosen from your OS's preference.
+When called in emacs lisp, if @fname is given, open that.
+URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
+Version 2019-11-04"
+    (interactive)
+    (let* (($file-list
+            (if @fname
+                (progn (list @fname))
+              (if (derived-mode-p major-mode 'dired-mode)
+                  (dired-get-marked-files)
+                (list (buffer-file-name)))))
+           ($do-it-p (if (<= (length $file-list) 5)
+                         t
+                       (y-or-n-p "Open more than 5 files? "))))
+      (when $do-it-p
+        (cond
+         ((string-equal system-type "darwin")
+          (mapc
+           (lambda ($fpath)
+             (message "%s" $fpath)
+             (shell-command
+              (concat "open " (shell-quote-argument $fpath))))
+           $file-list))
+         ((string-equal system-type "gnu/linux")
+          (mapc
+           (lambda ($fpath) (let ((process-connection-type nil))
+                         (start-process "" nil "xdg-open" $fpath)))
+           $file-list))))))
+
   (with-eval-after-load 'general
     (general-define-key :states '(normal)
                         :keymaps 'dired-mode-map
                         "l" 'dired-find-alternate-file
-                        "h"  'dired-up-directory)
+                        "h"  'dired-up-directory
+                        "C-<return>" 'xah-open-in-external-app)
     )
 
   ;; Colourful dired
@@ -117,8 +149,8 @@
         (general-define-key :states '(normal)
                             :keymaps 'dirvish-mode-map
                             "?" 'dirvish-menu-all-cmds))))
-    (use-package dirvish-extras
-      :straight nil)
+  (use-package dirvish-extras
+    :straight nil)
   )
 
 
