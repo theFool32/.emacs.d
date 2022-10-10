125c6c344443f6727657d5e4dd5e480e29b109c2
theFool32 <saber.rl32@gmail.com>
2022-10-10 20:34:20 +0800

    [magit] add `simple-git-commit-push`

--- elisp/init-magit.el
+++ elisp/init-magit.el
@@ -132,6 +132,29 @@ kill all magit buffers for this repo."
                                                 "https://\\2/\\3"
                                                 url)))
         (message "opening repo %s" url))))
+
+  (defun aborn/simple-git-commit-push ()
+    "Simple commit current git project and push to its upstream."
+    ;; (interactive "sCommit Message: ")
+    (interactive)
+    (when (and buffer-file-name
+               (buffer-modified-p))
+      (save-buffer))                   ;; save it first if modified.
+    (magit-diff-unstaged)
+    (when (yes-or-no-p "Do you really want to commit everything?")
+      (magit-stage-modified)
+      ;; (magit-mode-bury-buffer)
+      (magit-diff-staged)
+      (setq msg (read-string "Commit Message: "))
+      (when (= 0 (length msg))
+        (setq msg (format-time-string "commit by magit in emacs@%Y-%m-%d %H:%M:%S"
+                                      (current-time))))
+      (message "commit message is %s" msg)
+      ;;(magit-commit (list "-m" msg))
+      (magit-call-git "commit" "-m" msg)
+      (magit-push-current-to-upstream nil)
+      (message "now do async push to %s" (magit-get "remote" "origin" "url")))
+    (magit-mode-bury-buffer))
   )
 ;; -MagitPac
 
