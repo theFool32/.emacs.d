74728648e2bac317657742ac61969b033285bf85
theFool32 <saber.rl32@gmail.com>
2024-01-17 10:33:45 +0800

    improve: no need for `atomic-chrome`

--- init.el
+++ init.el
@@ -1820,13 +1820,6 @@ It handles the case of remote files as well."
 (use-package restart-emacs
   :commands restart-emacs)
 
-(use-package atomic-chrome
-  :defer
-  :commands (atomic-chrome-start-server)
-  :config
-  (setq atomic-chrome-url-major-mode-alist
-	    '(("overleaf\\.com" . LaTeX-mode))))
-
 (use-package tramp
   :defer 60
   :elpaca nil
