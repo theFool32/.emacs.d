cc47d96bd9deaf7c1ef6e303a60803853080dfcb
theFool32 <saber.rl32@gmail.com>
2024-07-19 19:52:19 +0800

    improve: remove unused `tabnine` package

--- init.el
+++ init.el
@@ -1754,31 +1754,6 @@ Quit if no candidate is selected."
   :config
   (defalias 'tabnine-capf 'tabnine-completion-at-point))
 
-(use-package tabnine
-  :disabled
-  :elpaca t
-  :commands (tabnine-start-process)
-  ;; :hook (prog-mode . tabnine-mode)
-  :diminish "⌬"
-  :custom
-  (tabnine-wait 1)
-  (tabnine-minimum-prefix-length 0)
-  :hook (kill-emacs . tabnine-kill-process)
-  :config
-  ;; (add-to-list 'completion-at-point-functions #'tabnine-completion-at-point)
-  (defalias 'tabnine-capf 'tabnine-completion-at-point)
-  (tabnine-start-process)
-  ;; :bind
-  ;; (:map  tabnine-completion-map
-  ;;    ("<tab>" . tabnine-accept-completion)
-  ;;    ("TAB" . tabnine-accept-completion)
-  ;;    ("M-f" . tabnine-accept-completion-by-word)
-  ;;    ("M-<return>" . tabnine-accept-completion-by-line)
-  ;;    ("C-g" . tabnine-clear-overlay)
-  ;;    ("M-[" . tabnine-previous-completion)
-  ;;    ("M-]" . tabnine-next-completion))
-  )
-
 (use-package tmux-capf
   :elpaca (:host github :repo "theFool32/tmux-capf" :files ("*.el" "*.sh"))
   :after cape
