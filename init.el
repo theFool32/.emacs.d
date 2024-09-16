5c5f519a10a94eb528a5ddfd3bde3715ee8dc538
theFool32 <saber.rl32@gmail.com>
2024-09-16 20:42:51 +0800

    update `consult` with `orderless`

--- init.el
+++ init.el
@@ -1451,8 +1451,8 @@ targets."
     (consult-ripgrep dir initial))
 
   ;; HACK add `ignore' according to upstream, wihout meaning
-  (defun consult--orderless-regexp-compiler (input type igore)
-    (setq input (orderless-pattern-compiler input))
+  (defun consult--orderless-regexp-compiler (input type &rest _config)
+    (setq input (cdr (orderless-compile input)))
     (cons
      (mapcar (lambda (r) (consult--convert-regexp r type)) input)
      (lambda (str) (orderless--highlight input t str))))
