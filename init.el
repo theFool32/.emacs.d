d3ef8c623e2483230e1ab1648563a1a3f0bcfa89
theFool32 <saber.rl32@gmail.com>
2024-12-18 11:33:26 +0800

    feat: use `colorful-mode` instead

--- init.el
+++ init.el
@@ -343,32 +343,6 @@ REST and STATE."
       (org-save-all-org-buffers)
     (save-buffer)))
 
-
-;;;###autoload
-(defun hexcolour-luminance (color)
-  "Calculate the luminance of a COLOR string (e.g. \"#ffaa00\", \"blue\").
-This is 0.3 red + 0.59 green + 0.11 blue and always between 0 and 255."
-  (let* ((values (x-color-values color))
-         (r (car values))
-         (g (cadr values))
-         (b (caddr values)))
-    (floor (+ (* .3 r) (* .59 g) (* .11 b)) 256)))
-
-;;;###autoload
-(defun hexcolour-add-to-font-lock ()
-  "Add color to font lock."
-  (interactive)
-  (font-lock-add-keywords
-   nil
-   `((,(concat "#[0-9a-fA-F]\\{3\\}[0-9a-fA-F]\\{3\\}?\\|"
-               (regexp-opt (x-defined-colors) 'words))
-      (0 (let ((colour (match-string-no-properties 0)))
-           (put-text-property
-            (match-beginning 0) (match-end 0)
-            'face `((:foreground ,(if (> 128.0 (hexcolour-luminance colour))
-                                      "white" "black"))
-                    (:background ,colour)))))))))
-
 ;;;###autoload
 (defvar +my/profiler--started nil)
 (defun +my/profiler-toggle ()
@@ -646,9 +620,6 @@ This is 0.3 red + 0.59 green + 0.11 blue and always between 0 and 255."
     (command-error-default-function data context caller)))
 
 (setq command-error-function #'filter-command-error-function)
-
-(dolist (hook '(conf-mode-hook conf-space-mode-hook emacs-lisp-mode-hook))
-  (add-hook hook #'hexcolour-add-to-font-lock))
 
 ;; (setq-default which-func-modes '(emacs-lisp-mode python-mode python-ts-mode org-mode latex-mode))
 ;; (which-function-mode +1)
@@ -3029,6 +3000,11 @@ kill all magit buffers for this repo."
   (doom-modeline-time-icon nil)
   )
 ;; -DoomModeline
+
+(use-package colorful-mode
+  :custom
+  (colorful-use-prefix t)
+  :hook ((prog-mode . colorful-mode)))
 
 ;;;; Pretty Code
 
