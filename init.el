ac5e825a3b4ec7a3de0bd49fcb66d70f21e0f7f9
theFool32 <saber.rl32@gmail.com>
2025-02-12 16:18:59 +0800

    refactor: remove unused code

--- init.el
+++ init.el
@@ -5080,32 +5080,6 @@ that do not have SCHEDULED or DEADLINE."
       "t" 'org-agenda-todo)
     )
 
-  (defun my:org-agenda-time-grid-spacing ()
-    "Set different line spacing w.r.t. time duration."
-    (save-excursion
-      (let* ((background (alist-get 'background-mode (frame-parameters)))
-             (background-dark-p (string= background "dark"))
-             (colors (if background-dark-p
-                         (list "#aa557f" "DarkGreen" "DarkSlateGray" "DarkSlateBlue")
-                       (list "#F6B1C3" "#FFFF9D" "#BEEB9F" "#ADD5F7")))
-             pos
-             duration)
-        (nconc colors colors)
-        (goto-char (point-min))
-        (while (setq pos (next-single-property-change (point) 'duration))
-          (goto-char pos)
-          (when (and (not (equal pos (point-at-eol)))
-                     (setq duration (org-get-at-bol 'duration)))
-            (let ((line-height (if (< duration 30) 1.0 (+ 0.5 (/ duration 60))))
-                  (ov (make-overlay (point-at-bol) (1+ (point-at-eol)))))
-              (overlay-put ov 'face `(:background ,(car colors)
-                                                  :foreground
-                                                  ,(if background-dark-p "white" "black")))
-              (setq colors (cdr colors))
-              (overlay-put ov 'line-height line-height)
-              (overlay-put ov 'line-spacing (1- line-height))))))))
-  ;; (add-hook 'org-agenda-finalize-hook #'my:org-agenda-time-grid-spacing)
-
   (run-with-timer 3 nil
                   (lambda ()
                     (with-eval-after-load 'org
