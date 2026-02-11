a62064f12174fd56054a80fcf2b486b1f099ff13
theFool32 <saber.rl32@gmail.com>
2026-02-12 01:31:24 +0800

    refactor: remove `+fold`

--- init.el
+++ init.el
@@ -5492,259 +5492,52 @@ kill the current timer, this may be a break or a running pomodoro."
 
 ;;;; Fold
 ;; Used for fold
-;; Copy from doom-emacs
-;;
-;; Helpers
 
-(defun +fold--ensure-hideshow-mode ()
-  (unless (bound-and-true-p hs-minor-mode)
-    (hs-minor-mode +1)))
-
-(defun +fold--vimish-fold-p ()
-  (and (featurep 'vimish-fold)
-       (cl-some #'vimish-fold--vimish-overlay-p
-                (overlays-at (point)))))
-
-(defun +fold--outline-fold-p ()
-  (and (or (bound-and-true-p outline-minor-mode)
-           (derived-mode-p 'outline-mode))
-       (outline-on-heading-p)))
-
-(defun +fold--hideshow-fold-p ()
-  (+fold--ensure-hideshow-mode)
-  (save-excursion
-    (ignore-errors
-      (or (hs-looking-at-block-start-p)
-          (hs-find-block-beginning)
-          (unless (eolp)
-            (end-of-line)
-            (+fold--hideshow-fold-p))))))
-
-(defun +fold--invisible-points (count)
-  (let (points)
-    (save-excursion
-      (catch 'abort
-        (if (< count 0) (beginning-of-line))
-        (while (re-search-forward hs-block-start-regexp nil t
-                                  (if (> count 0) 1 -1))
-          (unless (invisible-p (point))
-            (end-of-line)
-            (when (hs-already-hidden-p)
-              (push (point) points)
-              (when (>= (length points) count)
-                (throw 'abort nil))))
-          (forward-line (if (> count 0) 1 -1)))))
-    points))
-
-(defmacro +fold-from-eol (&rest body)
-  "Perform action after moving to the end of the line."
-  `(save-excursion
-     (end-of-line)
-     ,@body))
-
-
-;;
-;; Commands
-
-;;;###autoload
-(defun +fold/toggle ()
-  "Toggle the fold at point.
-
-Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
+(defun my/smart-fold-toggle ()
+  "Toggle fold based on the active minor mode."
   (interactive)
-  (save-excursion
-    (cond ((+fold--vimish-fold-p) (vimish-fold-toggle))
-          ((+fold--outline-fold-p) (outline-cycle))
-          ((+fold--hideshow-fold-p) (+fold-from-eol (hs-toggle-hiding))))))
+  (cond
+   ;; 如果启用了 treesit-fold，优先使用它 (Python, JS, Rust 等)
+   ((bound-and-true-p treesit-fold-mode)
+    (treesit-fold-toggle))
 
-;;;###autoload
-(defun +fold/open ()
-  "Open the folded region at point.
+   ;; 如果是 LaTeX 或 Elisp (通常使用 outline-minor-mode)
+   ((bound-and-true-p outline-minor-mode)
+    (outline-toggle-children))
 
-Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
-  (interactive)
-  (save-excursion
-    (cond ((+fold--vimish-fold-p) (vimish-fold-unfold))
-          ((+fold--outline-fold-p)
-           (outline-show-children)
-           (outline-show-entry))
-          ((+fold--hideshow-fold-p) (+fold-from-eol (hs-show-block))))))
+   ;; 最后的保底：使用 Evil 自带的折叠 (vimish-fold 或 hideshow)
+   (t
+    (evil-toggle-fold))))
 
-;;;###autoload
-(defun +fold/close ()
-  "Close the folded region at point.
-
-Targets `vimmish-fold', `hideshow', `ts-fold' and `outline' folds."
-  (interactive)
-  (save-excursion
-    (cond ((+fold--vimish-fold-p) (vimish-fold-refold))
-          ((+fold--hideshow-fold-p) (+fold-from-eol (hs-hide-block)))
-          ((+fold--outline-fold-p) (outline-hide-subtree)))))
-
-;;;###autoload
-(defun +fold/open-all (&optional level)
-  "Open folds at LEVEL (or all folds if LEVEL is nil)."
-  (interactive
-   (list (if current-prefix-arg (prefix-numeric-value current-prefix-arg))))
-  (cond ((featurep 'vimish-fold)
-         (vimish-fold-unfold-all))
-        ((save-excursion
-           (+fold--ensure-hideshow-mode)
-           (if (integerp level)
-               (progn
-                 (outline-hide-sublevels (max 1 (1- level)))
-                 (hs-life-goes-on
-                  (hs-hide-level-recursive (1- level) (point-min) (point-max))))
-             (hs-show-all)
-             (when (fboundp 'outline-show-all)
-               (outline-show-all)))))))
-
-;;;###autoload
-(defun +fold/close-all (&optional level)
-  "Close folds at LEVEL (or all folds if LEVEL is nil)."
-  (interactive
-   (list (if current-prefix-arg (prefix-numeric-value current-prefix-arg))))
-  (save-excursion
-    (progn
-      (when (featurep 'vimish-fold)
-        (vimish-fold-refold-all))
-      (+fold--ensure-hideshow-mode)
-      (hs-life-goes-on
-       (if (integerp level)
-           (hs-hide-level-recursive (1- level) (point-min) (point-max))
-         (hs-hide-all))))))
-
-;;;###autoload
-(defun +fold/next (count)
-  "Jump to the next vimish fold, outline heading or folded region."
-  (interactive "p")
-  (cl-loop with orig-pt = (point)
-           for fn
-           in (list (lambda ()
-                      (when (bound-and-true-p hs-block-start-regexp)
-                        (car (+fold--invisible-points count))))
-                    (lambda ()
-                      (when (featurep 'vimish-fold)
-                        (if (> count 0)
-                            (evil-vimish-fold/next-fold count)
-                          (evil-vimish-fold/previous-fold (- count))))
-                      (if (/= (point) orig-pt) (point))))
-           if (save-excursion (funcall fn))
-           collect it into points
-           finally do
-           (if-let* ((pt (car (sort points (if (> count 0) #'< #'>)))))
-               (goto-char pt)
-             (message "No more folds %s point" (if (> count 0) "after" "before"))
-             (goto-char orig-pt))))
-
-;;;###autoload
-(defun +fold/previous (count)
-  "Jump to the previous vimish fold, outline heading or folded region."
-  (interactive "p")
-  (+fold/next (- count)))
-
-(defface +fold-hideshow-folded-face
-  `((t (:inherit font-lock-comment-face :weight light)))
-  "Face to hightlight `hideshow' overlays."
-  :group 'doom-themes)
-
-;;;###autoload
-(defun +fold-hideshow-forward-block-by-indent-fn (_arg)
-  (let ((start (current-indentation)))
-    (forward-line)
-    (unless (= start (current-indentation))
-      (let ((range (+fold-hideshow-indent-range)))
-        (goto-char (cadr range))
-        (end-of-line)))))
-
-;;
-;; Indentation detection
-
-(defun +fold--hideshow-empty-line-p (_)
-  (string= "" (string-trim (thing-at-point 'line 'no-props))))
-
-(defun +fold--hideshow-geq-or-empty-p (base-indent)
-  (or (+fold--hideshow-empty-line-p base-indent)
-      (>= (current-indentation) base-indent)))
-
-(defun +fold--hideshow-g-or-empty-p (base-indent)
-  (or (+fold--hideshow-empty-line-p base-indent)
-      (> (current-indentation) base-indent)))
-
-(defun +fold--hideshow-seek (start direction before skip predicate base-indent)
-  "Seeks forward (if direction is 1) or backward (if direction is -1) from start, until predicate
-fails. If before is nil, it will return the first line where predicate fails, otherwise it returns
-the last line where predicate holds."
-  (save-excursion
-    (goto-char start)
-    (goto-char (point-at-bol))
-    (let ((bnd (if (> 0 direction)
-                   (point-min)
-                 (point-max)))
-          (pt (point)))
-      (when skip (forward-line direction))
-      (cl-loop while (and (/= (point) bnd) (funcall predicate base-indent))
-               do (progn
-                    (when before (setq pt (point-at-bol)))
-                    (forward-line direction)
-                    (unless before (setq pt (point-at-bol)))))
-      pt)))
-
-(defun +fold-hideshow-indent-range (&optional point)
-  "Return the point at the begin and end of the text block with the same (or
-greater) indentation. If `point' is supplied and non-nil it will return the
-begin and end of the block surrounding point."
-  (save-excursion
-    (when point
-      (goto-char point))
-    (let ((base-indent (current-indentation))
-          (begin (point))
-          (end (point)))
-      (setq begin (+fold--hideshow-seek begin -1 t nil #'+fold--hideshow-geq-or-empty-p base-indent)
-            begin (+fold--hideshow-seek begin 1 nil nil #'+fold--hideshow-g-or-empty-p base-indent)
-            end   (+fold--hideshow-seek end 1 t nil #'+fold--hideshow-geq-or-empty-p base-indent)
-            end   (+fold--hideshow-seek end -1 nil nil #'+fold--hideshow-empty-line-p base-indent))
-      (list begin end base-indent))))
-
-
-(use-package hideshow
-  :ensure nil
+(use-package treesit-fold
+  :ensure (:host github :repo "emacs-tree-sitter/treesit-fold")
+  :hook (prog-mode . treesit-fold-mode)
   :config
-  (setq hs-special-modes-alist
-        (append
-         '((yaml-ts-mode "\\s-*\\_<\\(?:[^:]+\\)\\_>"
-                         ""
-                         "#"
-                         +fold-hideshow-forward-block-by-indent-fn nil)
-           (latex-mode
-            ;; LaTeX-find-matching-end needs to be inside the env
-            ("\\\\begin{[a-zA-Z*]+}\\(\\)" 1)
-            "\\\\end{[a-zA-Z*]+}"
-            "%"
-            (lambda (_arg)
-              ;; Don't fold whole document, that's useless
-              (unless (save-excursion
-                        (search-backward "\\begin{document}"
-                                         (line-beginning-position) t))
-                (LaTeX-find-matching-end)))
-            nil))
-         hs-special-modes-alist
-         '((t)))))
-(use-package vimish-fold)
+  (setq treesit-fold-summary-format " <...%d lines...> ")
+  ;; 绑定到 Evil 标准键位
+  (with-eval-after-load 'evil
+    (define-key evil-normal-state-map (kbd "za") 'treesit-fold-toggle)
+    (define-key evil-normal-state-map (kbd "zc") 'treesit-fold-close)
+    (define-key evil-normal-state-map (kbd "zo") 'treesit-fold-open)
+    (define-key evil-normal-state-map (kbd "zM") 'treesit-fold-close-all)
+    (define-key evil-normal-state-map (kbd "zR") 'treesit-fold-open-all)))
+
+(use-package vimish-fold
+  :defer t
+  :init (vimish-fold-global-mode 1))
+
 (use-package evil-vimish-fold
-  :after evil
-  :bind
-  (([remap evil-toggle-fold]   . #'+fold/toggle)
-   ([remap evil-close-fold]    . #'+fold/close)
-   ([remap evil-open-fold]     . #'+fold/open)
-   ([remap evil-open-fold-rec] . #'+fold/open)
-   ([remap evil-close-folds]   . #'+fold/close-all)
-   ([remap evil-open-folds]    . #'+fold/open-all))
-  :commands (evil-vimish-fold/next-fold evil-vimish-fold/previous-fold
-                                        evil-vimish-fold/delete evil-vimish-fold/delete-all
-                                        evil-vimish-fold/create evil-vimish-fold/create-line)
-  :config
-  (vimish-fold-global-mode +1))
+  :after vimish-fold
+  :hook ((prog-mode conf-mode text-mode) . evil-vimish-fold-mode))
+
+(with-eval-after-load 'evil
+  (evil-define-key 'normal LaTeX-mode-map
+    (kbd "za") 'outline-toggle-children
+    (kbd "zc") 'outline-hide-subtree
+    (kbd "zo") 'outline-show-subtree
+    (kbd "zM") 'outline-hide-body
+    (kbd "zR") 'outline-show-all))
+
 ;; end of fold
 
 
@@ -5806,7 +5599,7 @@ begin and end of the block surrounding point."
 
   (general-evil-define-key 'normal
       '(python-mode-map python-ts-mode-map LaTeX-mode-map emacs-lisp-mode-map yaml-ts-mode-map)
-    "<tab>" '+fold/toggle)
+    "<tab>" 'my/smart-fold-toggle)
 
   ;; evil mode
   (general-def 'normal
